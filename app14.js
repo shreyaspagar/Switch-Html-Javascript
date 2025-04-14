ebeec884f3b48a0e537827340023216c03d9a826
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-04-14 19:11:52 +0530

    Add files via upload

---
+++ app14.js
@@ -0,0 +1,18 @@
+let color = "Red";
+
+switch(color) {
+    case  "Red":
+    console.log("stop");
+    break;
+    case "yellow":
+        console.log("slow down");
+        break;
+        case "green":
+            console.log("go");
+            break;
+            default:
+                console.log("light is broken");
+
+}
+
+console.log("after switch stt");
